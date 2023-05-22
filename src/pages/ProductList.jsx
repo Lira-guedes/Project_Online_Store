f4a3ce4b36461db6a4ed5ce6a790bffd036d8d1b
Lira Guedes <liracrg@gmail.com>
2023-05-22 20:56:12 -0300

    Requisito 5

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromQuery } from '../services/api';
 
 export default class productList extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      productCategory: [],
-    };
-  }
+  state = {
+    productCategory: [],
+    apiRequest: false,
+    name: '',
+  };
 
   async componentDidMount() {
     const productCategory = await getCategories();
     this.setState({ productCategory });
   }
 
+  saveName = ({ target }) => {
+    const { value } = target;
+    this.setState(() => ({
+      name: value,
+      savedName: value,
+    }));
+  };
+
+  // faz busca
+  handleSearch = async (event) => {
+    event.preventDefault();
+    const { name } = this.state;
+    console.log(name);
+    // requisição api
+    const api = await getProductsFromQuery(name);
+    this.setState({
+      apiRequest: api,
+    });
+  };
+
   render() {
-    const { productCategory } = this.state;
-    console.log(productCategory);
+    const { productCategory, apiRequest, savedName } = this.state;
     const list = productCategory.map(({ id, name }) => (
 
       <li
@@ -36,8 +53,49 @@ export default class productList extends Component {
     return (
       <>
         <label htmlFor="">
-          <input type="text" />
+          <input
+            type="text"
+            data-testid="query-input"
+            name="name"
+            // value={ savedName }
+            onChange={ this.saveName }
+          />
         </label>
+        <button
+          data-testid="query-button"
+          onClick={ this.handleSearch }
+        >
+          Pesquisar
+        </button>
+        {
+          apiRequest && apiRequest.length > 0 && (
+            <div>
+              <h2>
+                Resultado de álbuns de:
+                {' '}
+                {savedName}
+              </h2>
+              <ul>
+                {
+                  apiRequest.map((elem) => (
+                    <li key={ elem.id } data-testid="product">
+                      <img src={ elem.thumbnail } alt={ elem.title } />
+                      <h3>{ elem.title }</h3>
+                      <p>
+                        { elem.price }
+                        $
+                      </p>
+                    </li>
+                  ))
+                }
+              </ul>
+            </div>
+          )
+        }
+        {
+          apiRequest.length === 0 && (<p>Nenhum produto foi encontrado</p>)
+        }
+
         <p
           data-testid="home-initial-message"
         >
--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,6 @@ export async function getCategories() {
   // Implemente aqui
   const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
   const jsonData = await response.json();
-  console.log(jsonData);
   return jsonData;
 }
 
@@ -12,10 +11,17 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`);
   const jsonData = await response.json();
-  console.log(jsonData);
-  return jsonData;
+  return jsonData.results;
 }
 
+export async function getProductsFromQuery(query) {
+  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+  const jsonData = await response.json();
+  return jsonData.results;
+}
+
+// https://api.mercadolibre.com/sites/MLB/search?q=$QUERY
 // export async function getProductById() {
 //   // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
 //   // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
