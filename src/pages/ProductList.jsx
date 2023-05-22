8626bcb4accafa46fed3a608d184f944bce8f6ce
vinicius-virgilli <vinicius.virgilli3@gmail.com>
2023-05-22 18:27:45 -0300

    requisito 4

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 export default class productList extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      productCategory: [],
+    };
+  }
+
+  async componentDidMount() {
+    const productCategory = await getCategories();
+    this.setState({ productCategory });
+  }
+
   render() {
+    const { productCategory } = this.state;
+    console.log(productCategory);
+    const list = productCategory.map(({ id, name }) => (
+
+      <li
+        data-testid="category"
+        key={ id }
+      >
+        <a
+          href="test"
+        >
+          {name}
+
+        </a>
+      </li>
+    ));
     return (
       <>
         <label htmlFor="">
@@ -12,11 +42,11 @@ export default class productList extends Component {
           data-testid="home-initial-message"
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
-          Digite algum termo de pesquisa ou escolha uma categoria.
 
         </p>
         <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
 
+        {list}
       </>
     );
   }
