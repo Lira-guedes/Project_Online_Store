17286af99aada94e8384662be57dfa3a144c9db3
Lira Guedes <liracrg@gmail.com>
2023-05-24 16:54:35 -0300

    Requisito 10

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -10,9 +10,53 @@ export default class Cart extends Component {
     this.setState({ cart }/* , () => this.quantityProducts() */);
   }
 
+  // atualiza quantidade!
+  handleIncrease = (elem) => {
+    const { cart } = this.state;
+    const newCart = [];
+    cart.forEach((product) => {
+      if (product.id === elem.id) {
+        product.quantity += 1;
+      }
+      newCart.push(product);
+    });
+    // salva o cart atualizado
+    localStorage.setItem('cart', JSON.stringify(newCart));
+    this.setState({ cart: newCart });
+  };
+
+  // atualiza quantidade!
+  handleDecrease = (elem) => {
+    const newCart = [];
+    const { cart } = this.state;
+    if (elem.quantity > 1) {
+      cart.forEach((product) => {
+        if (product.id === elem.id) {
+          product.quantity -= 1;
+        }
+        newCart.push(product);
+      });
+      // salva o cart atualizado
+      localStorage.setItem('cart', JSON.stringify(newCart));
+      this.setState({ cart: newCart });
+    }
+  };
+
+  handleDelete = (elem) => {
+    const newCart = [];
+    const { cart } = this.state;
+    cart.forEach((product) => {
+      if (product.id !== elem.id) {
+        newCart.push(product);
+      }
+    });
+    // salva o cart atualizado
+    localStorage.setItem('cart', JSON.stringify(newCart));
+    this.setState({ cart: newCart });
+  };
+
   render() {
     const { cart } = this.state;
-    console.log(cart);
     let listProducts;
 
     if (cart) {
@@ -30,6 +74,24 @@ export default class Cart extends Component {
           <p data-testid="shopping-cart-product-quantity">
             {`Quantidade: ${product.quantity}`}
           </p>
+          <button
+            data-testid="product-increase-quantity"
+            onClick={ () => this.handleIncrease(product) }
+          >
+            +
+          </button>
+          <button
+            data-testid="product-decrease-quantity"
+            onClick={ () => this.handleDecrease(product) }
+          >
+            -
+          </button>
+          <button
+            data-testid="remove-product"
+            onClick={ () => this.handleDelete(product) }
+          >
+            X
+          </button>
         </li>
       ));
     }
